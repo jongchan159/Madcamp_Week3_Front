1525cf0e16ab12faec90efb49a9127761e9bee3f
jongc <jongchan159@gmail.com>
2024-07-15 13:48:06 +0900

    스토어 레이아웃 스토어 레이아웃 구성 및 어댑터 생성 #issue#3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
+    // naver login
+
 }
--- app/src/main/java/com/example/project3/Store/StoreAdapter.kt
+++ app/src/main/java/com/example/project3/Store/StoreAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.project3.Store
 
-class StoreAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project3.R
+
+class StoreAdapter (private var items: List<String>) : RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textName: TextView = view.findViewById(R.id.text_name)
+        val textPrice: TextView = view.findViewById(R.id.text_price)
+        val storeImage: ImageView = view.findViewById(R.id.store_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.store_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val storeItems = items[position]
+        holder.textName.text = storeItems[position].toString()
+        holder.textPrice.text = storeItems[position].toString()
+        // holder.storeImage. 이미지
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun updateItems(newItems: List<String>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
 }
